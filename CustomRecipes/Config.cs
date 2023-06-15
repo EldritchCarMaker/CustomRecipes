fbec6bae0627c6559d6cd357a83e5583a1e1362c
Nagorrogan <eldritchcarmaker@gmail.com>
2023-06-15 10:35:36 -0500

    Add project files.

---
+++ CustomRecipes/Config.cs
@@ -0,0 +1,71 @@
+﻿using Nautilus.Crafting;
+using Nautilus.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static CraftData;
+
+namespace CustomRecipes
+{
+    internal class Config : ConfigFile
+    {
+        public Dictionary<string, ConfigRecipeData> modifiedRecipes = new()
+        {
+            { 
+                TechType.Battery.ToString(), 
+                new ConfigRecipeData(new ConfigRecipeData.ConfigIngredient(TechType.AcidMushroom, 5), new ConfigRecipeData.ConfigIngredient(TechType.Copper, 2)) 
+            }
+        };
+    }
+    internal class ConfigRecipeData
+    {
+        public static implicit operator RecipeData(ConfigRecipeData configRecipeData)
+        {
+            RecipeData data = new RecipeData();
+            data.Ingredients = new();
+            configRecipeData.ingredients.ForEach(item => data.Ingredients.Add(item));
+            data.craftAmount = configRecipeData.craftAmount;
+            data.LinkedItems = new ();
+            configRecipeData.linkedItems.ForEach(item => data.LinkedItems.Add(item.AsEnum()));
+
+            return data;
+        }
+
+        [JsonConstructor()]
+        public ConfigRecipeData() { }
+
+        public ConfigRecipeData(params ConfigIngredient[] ingredients)
+        {
+            this.ingredients.AddRange(ingredients);
+        }
+
+        public int craftAmount = 1;
+        public List<ConfigIngredient> ingredients = new();
+        public List<string> linkedItems = new();
+
+
+        public class ConfigIngredient
+        {
+            [JsonConstructor()]
+            public ConfigIngredient() { }
+            public ConfigIngredient(TechType type, int count = 1)
+            {
+                techType = type.ToString();
+                this.count = count;
+            }
+            public ConfigIngredient(string type, int count = 1)
+            {
+                techType = type;
+                this.count = count;
+            }
+            public string techType = "None";
+            public int count = 0;
+
+            public static implicit operator Ingredient(ConfigIngredient configIngredient)
+            {
+                return new Ingredient(configIngredient.techType.AsEnum(), configIngredient.count);
+            }
+        }
+    }
+}
---
+++ CustomRecipes/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace CustomRecipes
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        internal new static Config Config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(10);//idfk random number just here so we can get 99% of modded techtypes
+            //not actually really necessary, most modded types are declared in Awake anyway
+            //but better to have than not have
+            Config.modifiedRecipes.ForEach(recipe => CraftDataHandler.SetRecipeData(recipe.Key.AsEnum(), recipe.Value));
+        }
+    }
+    public static class Extension
+    {
+        public static TechType AsEnum(this string str)
+        {
+            return AsEnum<TechType>(str);
+        }
+        public static T AsEnum<T>(this string str) where T : Enum
+        {
+            return (T)Enum.Parse(typeof(T), str);
+        }
+    }
+}
